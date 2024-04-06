d3e648fe512d235f63c28158257f2d4f49c57cb0
Flores Rodrigo <rodrigoef25@gmail.com>
2024-04-05 21:42:39 -0300

    Punto 1 del tp.

---
+++ tp1_FloresRodrigo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int a = 8, b = 5, c = 1, d = 20, e = 9;
+		double result;
+		result = (a + b + c + d + e) / 5.00;
+		System.out.println("El promedio es: " + result);
+	}
+
+}
