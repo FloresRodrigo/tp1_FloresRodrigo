1909a4c51bd79545df00868498d49e39f837846d
Flores Rodrigo <rodrigoef25@gmail.com>
2024-04-07 21:32:13 -0300

    Ejercicio 3 completo

--- tp1_FloresRodrigo/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ tp1_FloresRodrigo/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,10 +1,26 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un numero entero: ");
+		
+		int  num;
+		
+		num = scanner.nextInt();
+		
+		if((num % 2) == 0) {
+			num = num * 2;
+		} else {
+			num = num *3;
+		}
+		
+		System.out.println("Resultado: "+num);
+		scanner.close();
 	}
 
 }
