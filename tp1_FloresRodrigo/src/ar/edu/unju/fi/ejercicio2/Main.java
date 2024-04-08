8d1c23880e146327c81204a28d65533b962301d4
Flores Rodrigo <rodrigoef25@gmail.com>
2024-04-07 21:02:32 -0300

    Ejercicio 2 completo

---
+++ tp1_FloresRodrigo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		int edad = 23;
+		double alturaEdificio = 53.0;
+		float precioProducto = 2000.00f;
+		String numeroTelefono = "3885552388";
+		float calculoCoseno = 0.866f;
+		
+		System.out.println("Nombre de Pais: "+nombrePais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura de un edificio: "+alturaEdificio+"mts");
+		System.out.println("Precio de un producto: $"+precioProducto);
+		System.out.println("Numero de telefono: "+numeroTelefono);
+		System.out.println("Calculo del coseno de 0.5: "+calculoCoseno);
+	}
+
+}
